f97ee2e7b99bf4b2b2b36cffd3f9dea7dea7c2a2
qzlzdy <419286376@qq.com>
2020-05-13 14:18:39 +0800

    create KG_backend project

---
+++ Knowledge_Graph/KG_Backend/src/main/java/com/knowledgegraph/KgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.knowledgegraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgBackendApplication.class, args);
+	}
+
+}
---
+++ Knowledge_Graph/KG_Backend/src/test/java/com/knowledgegraph/KgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowledgegraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
